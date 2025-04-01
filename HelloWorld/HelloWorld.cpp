2b24596c66e81b7e07c334fc1233a32863597631
RedSousuke <rmeeks@student.neumont.edu>
2025-04-01 15:29:27 -0600

    Removed unnecessary comments

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,13 +1,10 @@
-// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
 using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
     cout << "Add this to git" << endl;
     printf("This works too");
-    // test commit
     return 0;
 }
