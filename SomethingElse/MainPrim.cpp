c9236155dd8ce442688ba8cfc006e990ce80fc78
RedSousuke <rmeeks@student.neumont.edu>
2025-04-01 22:50:29 -0600

    data types assignment

---
+++ SomethingElse/MainPrim.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+Person person;
+
+int main()
+{
+    const float TAX = 0.1f;
+    personMaker();
+
+    float totalHours = 0;
+    for (int i = 0; i < person.daysWorked; i++)
+    {
+        cout << "Hours worked on day " << (i + 1) << ":";
+        cin >> person.hoursPerDay[i];
+
+        totalHours += person.hoursPerDay[i];
+    }
+
+    float grossIncome = totalHours * person.wage;
+    float taxAmount = grossIncome * TAX;
+    float netIncome = grossIncome - taxAmount;
+
+    cout << "\nPayroll Summary\n";
+    cout << "-------------------------\n";
+    cout << "Name: " << person.name << " " << person.initial << ".\n";
+    cout << "Age: " << person.age << " (Is Adult: " << (person.isAdult ? "Yes" : "No") << ")\n";
+    cout << "Postal: " << person.zipcode << "\n";
+    cout << "Wage: $" << person.wage << "/hr\n";
+    cout << "Gross: $" << grossIncome << "\n";
+    cout << "Tax: $" << taxAmount << "\n";
+    cout << "Net Income: $" << netIncome << "\n";
+}
+
+void personMaker()
+{
+    cout << "Please provide name:\n";
+    cin >> person.name;
+
+    cout << "\nLast initial:\n";
+    cin >> person.initial;
+
+    cout << "\nAge:\n";
+    cin >> person.age;
+    person.isAdult << (person.age >= 18);
+
+    cout << "\nPostal code:\n";
+    cin >> person.zipcode;
+
+    cout << "\nHourly wage:\n";
+    cin >> person.wage;
+
+    cout << "\nDays of work (max 7):\n";
+    cin >> person.daysWorked;
+}
---
+++ SomethingElse/Person.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+class Person
+{
+	public:
+		std::string name;
+		char initial;
+		short age;
+		bool isAdult;
+		unsigned int zipcode;
+		float wage;
+		short daysWorked;
+		float hoursPerDay[7];
+};
+
